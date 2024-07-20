f3908396e3521053505a905c7e3ba1036114dfa2
fentoncollins123456789 <fentoncollins29@gmail.com>
2024-07-20 13:12:28 +0300

    initial commit

---
+++ me.js
@@ -0,0 +1,9 @@
+function openDrawer() {
+    document.getElementById("myDrawer").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeDrawer() {
+    document.getElementById("myDrawer").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
